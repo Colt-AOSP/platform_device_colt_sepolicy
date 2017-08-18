54f821540051d0e2046179d342b45b288742938c
Luca Stefani <luca020400@lineageos.org>
2017-08-18 10:35:47 +0200

    colt sepolicy: Initial repo structure
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

---
+++ common/sepolicy.mk
@@ -0,0 +1,7 @@
+#
+# This policy configuration will be used by all products that
+# inherit from Colt
+#
+
+BOARD_SEPOLICY_DIRS += \
+    device/colt/sepolicy/common
---
+++ qcom/sepolicy.mk
@@ -0,0 +1,7 @@
+#
+# This policy configuration will be used by all qcom products
+# that inherit from Colt
+#
+
+BOARD_SEPOLICY_DIRS += \
+    device/colt/sepolicy/qcom
