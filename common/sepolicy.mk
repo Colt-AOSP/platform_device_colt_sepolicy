f50c67fcc45c212134b98e4e826e2ff883b0d83c
Sagar Shah <sagarshah2326@gmail.com>
2018-03-14 16:17:41 +0530

    Colt sepolicy: Adapt with Lineage
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -2,6 +2,11 @@
 # This policy configuration will be used by all products that
 # inherit from Colt
 #
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    device/colt/sepolicy/common/public
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/colt/sepolicy/common/private
 
 BOARD_SEPOLICY_DIRS += \
-    device/colt/sepolicy/common
+    device/colt/sepolicy/common/vendor
